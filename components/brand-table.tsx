1c0e3cca04244609169245bdee92d4caac2059b3
ljj <ljj3347@naver.com>
2025-04-23 00:26:51 +0900

    123

--- components/brand-table.tsx
+++ components/brand-table.tsx
@@ -129,6 +129,7 @@ export default function BrandTable() {
                   setCurrentPage(1);
                 }}
               />
+              
             </div>
             <Dialog open={isAddModalOpen} onOpenChange={setIsAddModalOpen}>
               <DialogTrigger asChild>
